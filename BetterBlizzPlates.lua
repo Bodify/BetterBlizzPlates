a383b22590feba9fea12f3108157b630cf75b1f3
Body <bodifyttv@gmail.com>
2023-12-21 10:59:32 +0100

    nahj profile slash command

--- BetterBlizzPlates.lua
+++ BetterBlizzPlates.lua
@@ -2166,6 +2166,8 @@ SLASH_BBP1 = "/bbp"
 SlashCmdList["BBP"] = function(msg)
     if msg == "news" then
         NewsUpdateMessage()
+    elseif msg == "nahj" then
+        StaticPopup_Show("BBP_CONFIRM_NAHJ_PROFILE")
     else
         InterfaceOptionsFrame_OpenToCategory(BetterBlizzPlates)
     end
