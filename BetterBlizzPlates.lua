a0348928649fcbd290712eaaf5e12c0b567ab978
Body <bodifyttv@gmail.com>
2024-01-04 17:44:04 +0100

    ver

--- BetterBlizzPlates.lua
+++ BetterBlizzPlates.lua
@@ -11,7 +11,7 @@ LSM:Register("statusbar", "Shattered DF (BBP)", [[Interface\Addons\BetterBlizzPl
 LSM:Register("font", "Yanone (BBP)", [[Interface\Addons\BetterBlizzPlates\media\YanoneKaffeesatz-Medium.ttf]])
 
 local addonVersion = "1.00" --too afraid to to touch for now
-local addonUpdates = "1.3.1"
+local addonUpdates = "1.3.2"
 local sendUpdate = true
 BBP.VersionNumber = addonUpdates
 local _, playerClass
@@ -603,7 +603,7 @@ local function SendUpdateMessage()
         C_Timer.After(7, function()
             --bbp news
             DEFAULT_CHAT_FRAME:AddMessage("|A:gmchat-icon-blizz:16:16|a Better|cff00c0ffBlizz|rPlates " .. addonUpdates .. ":")
-            DEFAULT_CHAT_FRAME:AddMessage("|A:gmchat-icon-blizz:16:16|a Enemy nameplate custom colors. Guild name setting. Misc settings category.")
+            DEFAULT_CHAT_FRAME:AddMessage("|A:gmchat-icon-blizz:16:16|a Minor bugfixes. Reminder to please let me know if you run into any bugs so I can fix em :)")
         end)
     end
 end
