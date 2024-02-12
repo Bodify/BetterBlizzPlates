bc705b7a9ebaa383214de14d198ca44747611b4f
anullog <git@anullog.com>
2024-02-12 20:42:30 +0100

    make files lowercase

---
+++ core/boot.lua
@@ -0,0 +1,6 @@
+-- boot.lua handles the initialisation of the addon and the creation of the root module.
+
+local addonName = ... ---[[@type string, table]]
+
+---@class BetterBlizzPlates: AceModule
+local addon = LibStub("AceAddon-3.0"):NewAddon(addonName, 'AceHook-3.0')
